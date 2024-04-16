aee2a11368ea17c750f05b3e6e6a135fd398d831
felipe <felipe.sanchez@apiumhub.com>
2024-04-16 12:23:34 +0200

    First commit

---
+++ src/main/java/com/apium/legacy_kata/LegacyKataApplication.java
@@ -0,0 +1,13 @@
+package com.apium.legacy_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyKataApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LegacyKataApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/apium/legacy_kata/LegacyKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apium.legacy_kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyKataApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
