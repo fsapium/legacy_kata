8138eff29c7888bd8216712874e21358e774836d
felipe <felipe.sanchez@apiumhub.com>
2024-04-16 15:40:43 +0200

    Added functionality for conjured items

--- src/test/java/com/apium/legacy_kata/GildedRoseTest.java
+++ src/test/java/com/apium/legacy_kata/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.apium.legacy_kata;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,57 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
   @Test
-  void foo() {
-    Item[] items = new Item[] { new Item("foo", 0, 0) };
-    GildedRose app = new GildedRose(items);
-    app.updateQuality();
-    assertEquals("fixme", app.items[0].name);
+  @DisplayName("Quality should lower one point if sellIn > 0")
+  void should_update_quality() {
+    Item[] items = new Item[] { new Item("foo", 1, 2) };
+    GildedRose sut = new GildedRose(items);
+    sut.updateQuality();
+    assertEquals(1, sut.items[0].quality);
+  }
+
+  @Test
+  @DisplayName("Quality should lower two points if sellIn = 0")
+  void should_update_quality_faster() {
+    Item[] items = new Item[] { new Item("foo", 0, 2) };
+    GildedRose sut = new GildedRose(items);
+    sut.updateQuality();
+    assertEquals(0, sut.items[0].quality);
+  }
+
+  @Test
+  @DisplayName("Should update sellIn property one point")
+  void should_update_sellIn() {
+    Item[] items = new Item[] { new Item("foo", 1, 2) };
+    GildedRose sut = new GildedRose(items);
+    sut.updateQuality();
+    assertEquals(0, sut.items[0].sellIn);
+  }
+
+  @Test
+  @DisplayName("Should lower two points for conjured items")
+  void should_update_quality_for_conjured_items() {
+    Item[] items = new Item[] { new Item("Conjured foo", 2, 3) };
+    GildedRose sut = new GildedRose(items);
+    sut.updateQuality();
+    assertEquals(1, sut.items[0].quality);
+  }
+
+  @Test
+  @DisplayName("Quality should always be bigger than zero for conjured items")
+  void quality_bigger_than_zero_for_conjured_items() {
+    Item[] items = new Item[] { new Item("Conjured foo", 2, 1) };
+    GildedRose sut = new GildedRose(items);
+    sut.updateQuality();
+    assertEquals(0, sut.items[0].quality);
+  }
+
+  @Test
+  @DisplayName("When sellIn is lower or equal than zero should lower quality faster")
+  void should_update_quality_faster_for_conjured_items() {
+    Item[] items = new Item[] { new Item("Conjured foo", 1, 4) };
+    GildedRose sut = new GildedRose(items);
+    sut.updateQuality();
+    assertEquals(0, sut.items[0].quality);
   }
 
 }
